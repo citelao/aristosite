12ed5780b1079ef66eb3310f1c6db4212293007e
Ben <ben@stolovitz.com>
2015-07-31 15:32:15 -0400

    mobile menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,3 +5,13 @@ console.log(
 
 console.log('There\'s no real call for JS on this page so I\'m just gonna log to the console.');
 console.log('meow meow meow meow meow meow');
+
+function showMenu() {
+  var menu = document.getElementById("nav");
+
+  if(menu.classList.contains("navigation--toggled")) {
+    menu.classList.remove("navigation--toggled");
+  } else {
+    menu.classList.add("navigation--toggled");
+  }
+}
