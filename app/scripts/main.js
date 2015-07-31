1a9d64df0628cb7409445ef10fb1eec31cbc8775
Ben <ben@stolovitz.com>
2015-07-30 23:19:34 -0400

    Working out content. Still don't like homepage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+console.log(
+  '%cThe Aristocats like web developers. You should join us. 😻😻',
+  'font-weight: bold;'
+);
+
+console.log('There\'s no real call for JS on this page so I\'m just gonna log to the console.');
+console.log('meow meow meow meow meow meow');
